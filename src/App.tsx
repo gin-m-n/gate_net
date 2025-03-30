2f2d034a8e3e291552538fa4c3bfc757d2df907d
takaodaze <gin2798@gmail.com>
2025-03-31 04:51:11 +0900

    uninstall three.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import { Canvas, } from '@react-three/fiber'
-import { AnimatedBox } from './AnimatedBox'
-import { WindowManager } from './WindowManager'
-
 function App() {
   return (
-    <div id="canvas-container">
-      <Canvas orthographic camera={{ position: [0, 0, 1000], far: 2000 }} >
-        <AnimatedBox />
-        <WindowManager />
-      </Canvas>
-    </div>
+    <div></div>
   )
 }
 
--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export const convertCoordinate = (w: number, h: number, docX: number, docY: number) => {
-  const x = docX - w / 2
-  const y = -(docY - h / 2)
-  return [x, y]
-}
